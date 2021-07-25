9d2e7498df80732353973a18322cb406cc9f97f2
sayyr1 <edwinsay03@gmail.com>
2021-07-24 21:11:23 -0500

    pages

---
+++ src/App4.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import {Route, Switch} from "react-router-dom";
+import Home from './pages/home'
+import Abouts from "./pages/about";
+import Menu from "./pages/menu";
+import Dropdown from "./components/Dropdown";
+
+function App3() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () =>{
+        setIsOpen(!isOpen)
+    };
+    useEffect(() => {
+        const hideMenu = () =>{
+            if (window.innerWidth > 768 && isOpen){
+                setIsOpen(false)
+            }
+        };
+        
+        window.addEventListener('resize', hideMenu);
+        return () =>{
+            window.removeEventListener('resize', hideMenu)
+        };
+        
+    });
+    
+    
+    return (
+        <>
+            <Navbar toggle={toggle}/>
+            <Dropdown isOpen = {isOpen} toggle={toggle}/>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/menu" component={Menu}/>
+                <Route path="/about" component={Abouts}/>
+            </Switch>
+            <Footer/>
+        </>
+    )
+}
+
+export default App3
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Principla from "../components/Principla";
+import Contents from "../components/Contents";
+
+const Home = () =>{
+    return(
+        <>
+            <Principla/>
+            <Contents/>
+        </>
+    )
+}
+export default Home
