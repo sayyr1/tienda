353f1cbe99e938497c1629295b7d9d5f05a75d5a
Snowpack Build <actions@github.com>
2021-07-25 01:41:18 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -5,7 +5,9 @@ import React from "../snowpack/pkg/react.js";
 import ReactDOM from "../snowpack/pkg/react-dom.js";
 import App from "./App.js";
 import "./index.css.proxy.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null)), document.getElementById("root"));
+ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null), /* @__PURE__ */ React.createElement("p", {
+  className: "text-red-500"
+}, "hola no se que estas haciendo")), document.getElementById("root"));
 if (undefined /* [snowpack] import.meta.hot */ ) {
   undefined /* [snowpack] import.meta.hot */ .accept();
 }
