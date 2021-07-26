b36b4712d022d23816b0067338cda4334b263d59
sayyr1 <edwinsay03@gmail.com>
2021-07-26 13:12:16 -0500

    pages

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import flower3 from "../images/flower1.jpg"
-import flower2 from "../images/flower2.jpg"
+// import flower3 from "../images/flower1.jpg"
+// import flower2 from "../images/flower2.jpg"
 const Contents = () => {
     return(
         <>
         <div className="menu-card">
-            <img src={flower3} alt="egg" className="h-full rounded mb-20 shadow"/>
+            <img src={"../images/flower1.jpg"} alt="egg" className="h-full rounded mb-20 shadow"/>
             <div className="center-content">
                 <h2 className="text-2xl mb-2">Flores</h2>
                 <p className="mb-2">Bonitas flores y coloridas</p>
@@ -13,7 +13,7 @@ const Contents = () => {
             </div>
         </div>
     <div className="menu-card">
-        <img src={flower2} alt="egg" className="h-full rounded mb-20 shadow"/>
+        <img src={"../images/flower2.jpg"} alt="egg" className="h-full rounded mb-20 shadow"/>
         <div className="center-content">
             <h2 className="text-2xl mb-2">Flores</h2>
             <p className="mb-2">Bonitas flores y coloridas</p>
